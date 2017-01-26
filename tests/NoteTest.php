e394edb9a6cdc27ff8ff97a63bec17cd1103f7cb
hberumen <hberumen@gmail.com>
2017-01-25 19:24:37 -0600

    first commit

---
+++ tests/NoteTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class NoteTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_notes_list()
+    {
+        $this->assertTrue(true);
+    }
+}
